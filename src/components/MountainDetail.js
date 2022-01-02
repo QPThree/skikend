164053a72dc4cb77c423cf597b0b2105967cdf68
Quentin Young <youngqp3@gmail.com>
2022-01-01 21:20:12 -0800

    added progress bars to mountain detail

--- src/components/MountainDetail.js
+++ src/components/MountainDetail.js
@@ -1,33 +1,38 @@
 import React from 'react'
 import { useEffect } from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, ProgressBar } from 'react-bootstrap'
 
 function MountainDetail({ data }) {
 
     useEffect(() => {
-       
+
     }, [])
 
     const renderCards = () => {
         const cards = data.forecast.map((day, indx) => {
             let year = day.date.slice(-4)
-            let month = day.date.slice(3,5)
-            let d = day.date.slice(0,2)
-            let date = new Date(year,month -1,d)
-            return (<Card className="shadow p-3 mb-5 rounded" style={{backgroundColor:"#dee2e6"}} key={indx}>
+            let month = day.date.slice(3, 5)
+            let d = day.date.slice(0, 2)
+            let date = new Date(year, month - 1, d)
+            return (<Card className="shadow p-3 mb-5 rounded" style={{ backgroundColor: "#dee2e6" }} key={indx}>
                 <Card.Title>{day.base.wx_desc}</Card.Title>
                 <Card.Body>
+
                     Time: {day.time}
-                    <br/>
-                    Base Temp: {day.base.temp_f}
-                    <br/>
-                    Snow: {day.precip_in}"
-                    <br/>
-                    Wind Speed: {day.base.windspd_mph}
-                    <br/>
-                    Visibility: {day.vis_mi} miles
+                    <br />
+                    <ProgressBar variant="info" label={`${day.base.temp_f} \xB0`} now={(day.base.temp_f / 40) * 100} />
+
+                    <br />
+                    <ProgressBar  variant="info" label={`Snow ${day.precip_in} \"`} now={(day.precip_in / 6) * 100} />
+
+                    <br />
+                    <ProgressBar  variant="info"  label={`${day.base.windspd_mph} mph`} now={(day.base.windspd_mph / 20) * 100} />
+
+                    <br />
+                    <ProgressBar  variant="info" label={`Visibility ${day.vis_mi} miles`} now={(day.vis_mi / 6) * 100} />
+                   
                 </Card.Body>
-                <Card.Footer style={{backgroundColor:"#3d405b", color:"#fff"}}>
+                <Card.Footer style={{ backgroundColor: "#3d405b", color: "#fff" }}>
                     {date.toDateString()}
                 </Card.Footer>
             </Card>)
@@ -38,7 +43,7 @@ function MountainDetail({ data }) {
 
     return (
         <>
-           <h3 style={{fontFamily:"Moon Dance", color:"#343a40"}}> {data.name}</h3>
+            <h3 style={{ fontFamily: "Moon Dance", color: "#343a40" }}> {data.name}</h3>
             {renderCards()}
         </>
     )
